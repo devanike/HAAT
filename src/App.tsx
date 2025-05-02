3f44816e6735823df2c72e32c2d5f5108664b015
EnnTek345 <enntech345@gmail.com>
2025-05-03 00:35:43 +0100

    fix login form

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/index';
 
 import Login from './pages/Login';
-=======
+
 // import About from './pages/about'
 
 function App() {
